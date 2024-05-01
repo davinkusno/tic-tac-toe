58b0e035768edfe622e6d247bbca6e7de0522dcf
Davin <davinkusno@gmail.com>
2024-05-01 21:32:44 +0700

    Init commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,39 @@
+import { act, useState } from 'react'
+
+export default function GameBoard({ board, onSelectSquare }) {
+	// const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+	// function handleSelectSquare(rowIndex, colIndex) {
+	// 	if (!gameBoard[rowIndex][colIndex]) {
+	// 		setGameBoard((prevGameBoard) => {
+	// 			const updatedBoard = [
+	// 				...prevGameBoard.map((innerArray) => [...innerArray]),
+	// 			]
+	// 			updatedBoard[rowIndex][colIndex] = activePlayerSymbol
+	// 			return updatedBoard
+	// 		})
+
+	// 		onSelectSquare()
+	// 	}
+	// }
+
+	return (
+		<ol id='game-board'>
+			{board.map((row, rowIndex) => (
+				<li key={rowIndex}>
+					<ol>
+						{row.map((playerSymbol, colIndex) => (
+							<li key={colIndex}>
+								<button
+									onClick={() => onSelectSquare(rowIndex, colIndex)}
+									disabled={playerSymbol !== null}>
+									{playerSymbol}
+								</button>
+							</li>
+						))}
+					</ol>
+				</li>
+			))}
+		</ol>
+	)
+}
---
+++ src/components/Player.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+export default function Player({
+	initialName,
+	symbol,
+	isActive,
+	onChangeName,
+}) {
+	const [playerName, setPlayerName] = useState(initialName)
+	const [isEditing, setIsEditing] = useState(false)
+
+	function handleEditClick() {
+		setIsEditing((editing) => !editing)
+
+		if (isEditing) {
+			onChangeName(symbol, playerName)
+		}
+	}
+
+	function handleChange(event) {
+		setPlayerName(event.target.value)
+	}
+
+	return (
+		<li className={isActive ? 'active' : undefined}>
+			<span className='player'>
+				{!isEditing ? (
+					<span className='player-name'>{playerName}</span>
+				) : (
+					<input
+						type='text'
+						required
+						value={playerName}
+						onChange={handleChange}
+					/>
+				)}
+				<span className='player-symbol'>{symbol}</span>
+			</span>
+			<button onClick={handleEditClick}>{!isEditing ? 'edit' : 'save'}</button>
+		</li>
+	)
+}
